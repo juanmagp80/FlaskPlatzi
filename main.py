e9847359683814f469e15d53ee315dd11b610b53
Juanma Garrido <juangpdev@gmail.com>
2024-04-17 23:56:34 +0200

    cambios

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response, redirect, render_template
 
 app = Flask(__name__)
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
 
 @app.route('/')
 def index ():
@@ -11,9 +12,13 @@ def index ():
 @app.route('/hello')
 def hello ():
     user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip':user_ip, 
+        'todos':todos
+    }
 
     user_ip= request.remote_addr
-    return render_template('hello.html', user_ip=user_ip)
+    return render_template('hello.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
