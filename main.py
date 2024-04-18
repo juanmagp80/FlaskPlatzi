53f403061ce84892cf4c1ada46889108d06fc233
John Doe <johndoe@example.com>
2024-04-18 14:11:11 +0200

    capitulo manejo de errores

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-from flask import Flask, request, Response, redirect, render_template
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
 
 @app.route('/')
-def index ():
-    user_ip= request.remote_addr
-    response = make_response = (redirect('/hello'))
+def index():
+    user_ip = request.remote_addr
+    response = redirect('/hello')
     response.set_cookie('user_ip', user_ip) 
     return response
+
 @app.route('/hello')
-def hello ():
+def hello():
     user_ip = request.cookies.get('user_ip')
     context = {
-        'user_ip':user_ip, 
-        'todos':todos
+        'user_ip': user_ip, 
+        'todos': todos
     }
-
-    user_ip= request.remote_addr
+    print(context)
     return render_template('hello.html', **context)
 
 if __name__ == '__main__':
