00ddf4d547a64d0f0f705f9cbdc0da9e6df67012
John Doe <johndoe@example.com>
2024-04-19 14:15:16 +0200

    tests

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from flask import session
+from flask import flash
+from flask import url_for
+from flask import make_response
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'tu-clave-secreta'
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 bootstrap = Bootstrap(app)
 
@@ -17,20 +21,31 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-@app.errorhandler(404)
 
+@app.cli.command()
+def test (): 
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
 
 def not_found(error):
     return render_template('404.html', error=error)
 
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
-    response = redirect('/hello')
-    response.set_cookie('user_ip', user_ip) 
+
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
+
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = request.cookies.get('user_ip')
     login_form = LoginForm()
@@ -40,7 +55,14 @@ def hello():
         'login_form': login_form
         
     }
-    print(context)
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con Ã©xito!')
+
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context)
 
 if __name__ == '__main__':
